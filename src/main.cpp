2e8594bba126a56959974b8b6a5f3981a6c4d8a2
knight <karvin_jimenez@hotmail.com>
2024-07-08 23:03:39 -0400

    added assets and working in the gameScreen.

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@ int main()
 
     Player player = Player(screenWidth / 2, screenHeight / 2);
 
+    Texture2D background = LoadTexture("assets/images/background-day.png");
+
+    Texture2D ground = LoadTexture("assets/images/base.png");
+
     InitAudioDevice();
 
     Sound hitSound = LoadSound("assets/sounds/okay.wav");
@@ -26,6 +30,16 @@ int main()
         ClearBackground(Color{0, 0, 0, 0});
 
         player.Draw();
+
+        DrawTexture(background, 0, 0, WHITE);
+        DrawTexture(background, background.width, 0, WHITE);
+        DrawTexture(background, background.width * 2, 0, WHITE);
+        DrawTexture(background, background.width * 3, 0, WHITE);
+
+        DrawTexture(ground, 0, screenHeight - ground.height, WHITE);
+        DrawTexture(ground, ground.width, screenHeight - ground.height, WHITE);
+        DrawTexture(ground, ground.width * 2, screenHeight - ground.height, WHITE);
+        DrawTexture(ground, ground.width * 3, screenHeight - ground.height, WHITE);
 
         EndDrawing();
     }
